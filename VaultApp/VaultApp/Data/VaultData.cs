7181754f4df363956bd3fa051bd63a96dd377aff
SaiKiran Palle <saikiranpalle@MacBook-Pro.local>
2022-05-16 22:52:22 -0400

    adding files

---
+++ VaultApp/VaultApp/Data/VaultData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaultApp.Data
+{
+    public static class VaultData
+    {
+        //this class can be connected to the database to pull data from dataTable, just mocked for local host settings. 
+        public static List<string> Data()
+        {
+            return new List<string>()
+            {
+                "APPLEDB",
+                "MACDB"
+            };
+        }
+
+    }
+}
--- VaultApp/VaultApp/VaultApp.Designer.cs
+++ VaultApp/VaultApp/VaultApp.Designer.cs
@@ -29,21 +29,48 @@ namespace VaultApp
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(2255, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(207, 81);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(94, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // VaultApp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(2519, 1001);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
             this.Name = "VaultApp";
             this.Text = "Vault";
             this.Load += new System.EventHandler(this.VaultApp_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
